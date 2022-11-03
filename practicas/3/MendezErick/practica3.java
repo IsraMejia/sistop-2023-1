fdacdecabbc70591dbc1a51b62d9294d24595aac
IsraMejia <ihmaisrael@gmail.com>
2022-11-03 00:19:46 -0600

    vuelvo a probar traer cambios desde github

--- practicas/3/MendezErick/practica3.java
+++
@@ -1,6 +0,0 @@
-public class practica3{
-    public static void main(String []args){
-        System.out.println("\n Méndez Sánchez Erick Jair");
-        System.out.println("Esta es mi prueba de la práctica 3");
-    }
-}
